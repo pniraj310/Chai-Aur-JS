df3ff078ad65fda93a5751c86d8da644c5a1ec5c
Niraj <pniraj310@gmail.com>
2025-12-17 21:57:28 +0530

    this is the updated file till now coversion operations

---
+++ 01-Basics/conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "33"
+
+
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+console.log(typeof(valueInNumber))
+
+let score2 = "33abc"
+
+console.log(typeof(score2))
+
+let valueInNumber2 = Number(score2)
+console.log(valueInNumber2)
+console.log(typeof(valueInNumber2))
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+
